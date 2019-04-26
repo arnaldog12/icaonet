5ff02e5e5637af24913c06fa738b00634af727b0
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2019-04-26 14:14:33 -0300

    update main

--- submission/FVCSubmission/src/main.cpp
+++ submission/FVCSubmission/src/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
 #include "FvcHeader.h"
-#include "MRKFile.h"
+#include "OutputFile.h"
+#include "FileUtils.h"
+#include "opencv2/highgui/highgui.hpp"
+
+bool checkImageFormat(std::string fileName);
 
 int main(int argc, char *argv[]) 
 {
-	MRKFile *mrkFile = new MRKFile();
-	std::cout << mrkFile->toString() << std::endl;
+	int retVal = SUCCESS;
+	if (argc != 3) retVal = NO_MORE_INFO;
 
-	return 0;
+	std::string faceImageFile(argv[1]);
+	std::string outputFile(argv[2]);
+	OutputFile *outFile = new OutputFile(outputFile);
+
+	if (!checkImageFormat(faceImageFile)) retVal = UNSUPPORTED_IMAGE_FORMAT;
+
+	cv::Mat im = cv::imread(faceImageFile, cv::IMREAD_ANYCOLOR);
+	if (im.empty()) retVal = UNUSEFUL_IMAGE_CONTENT;
+
+	if (retVal != SUCCESS)
+		outFile->write(faceImageFile, retVal);
+	else
+	{
+		Eye *rightEye = new Eye();
+		Eye *leftEye = new Eye();
+		PhotographicRequirements *reqs = new PhotographicRequirements();
+
+
+
+		delete rightEye;
+		delete leftEye;
+		delete reqs;
+	}
+	
+	outFile->close();
+	delete outFile;
+	return retVal;
+}
+
+bool checkImageFormat(std::string fileName)
+{
+	std::string ext = FileUtils::getFileExtension(fileName);
+	std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+	return ext.compare("png") == 0 || ext.compare("jpg") == 0 || ext.compare("bmp") == 0;
 }
