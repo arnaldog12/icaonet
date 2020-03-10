50002a9e98da3b83650cab5bc0f63e0a8828db43
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2020-03-09 21:22:44 -0300

    add new vars to global_config

--- notebooks/global_config.py
+++ notebooks/global_config.py
@@ -1,2 +1,9 @@
 FOLDER_IMAGES = '../data/pybossa/images/'
 FOLDER_MRKS = '../data/pybossa/ground_truth/1/'
+FOLDER_DATA_AUGMENTATION = '../data/pybossa/augmentation/'
+
+FILE_DUMP = '../data/pybossa/pybossa.pkl'
+
+IMAGE_SIZE = (224, 224)
+
+RANDOM_SEED = 42
