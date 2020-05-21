b2b60cf3ff5d588b1aa8739c92c9ac1980110190
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2020-05-21 14:27:33 -0300

    move autoencoder notebooks to corresponding folders
    [to fix] update paths in local_config.py

---
+++ notebooks/autoencoder/local_config.py
@@ -0,0 +1,13 @@
+# training params
+USER_IDS = "best_eer"
+IMAGE_SIZE = (224, 224)
+RANDOM_SEED = 42
+
+# folders
+FOLDER_IMAGES = '../data/pybossa/images/'
+FOLDER_MRKS = f'../data/pybossa/ground_truth/{USER_IDS}/'
+FOLDER_MODELS = '../models/autoencoders_requirements/'
+
+# files
+FILE_DUMP_IMAGES = '../data/pybossa/pybossa_images.pkl'
+FILE_DUMP_MRKS = '../data/pybossa/pybossa_mrks.pkl'
