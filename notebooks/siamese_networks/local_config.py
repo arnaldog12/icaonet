3675ad23734cba722c8e45cd156a2b10d4f148e1
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2020-06-22 19:00:58 -0300

    submit v0.5.6 to FVC
    train classification branch with labeled data instead of pos/neg dataset
    
    - add dump files to local_config.py

--- notebooks/siamese_networks/local_config.py
+++ notebooks/siamese_networks/local_config.py
@@ -1,3 +1,7 @@
 # folders
 FOLDER_IMAGES = "../../data/siamese_networks"
 FOLDER_MODELS = "../../models/siamese_networks"
+
+# files
+FILE_DUMP_IMAGES = '../../data/cropped_faces/cropped_faces_images.pkl'
+FILE_DUMP_MRKS = '../../data/cropped_faces/cropped_faces_mrks.pkl'
