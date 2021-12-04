fda4a301fde6fba71df86412363e59c11bdd8a43
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2021-12-04 17:37:44 -0300

    generate a dataset with ink marked/creased images

--- notebooks/global_config.py
+++ notebooks/global_config.py
@@ -7,5 +7,6 @@ FOLDER_IMAGES = 'data/cropped_faces/images/'
 FOLDER_MRKS = 'data/cropped_faces/ground_truth/'
 
 # files
+FILE_DATASET = 'data/inkmarked_creased/dataset.pkl'
 FILE_DUMP_IMAGES = 'data/cropped_faces/cropped_faces_images.pkl'
 FILE_DUMP_MRKS = 'data/cropped_faces/cropped_faces_mrks.pkl'
