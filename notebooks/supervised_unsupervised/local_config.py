367e1e2128b11bdd7fc272c87a038dd15b197529
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2021-12-12 16:23:40 -0300

    submit v0.8.2 to FVC
    the same model of v0.6.4, but with artificially generated images for ink marked/creased requirement
    
    v0.8.1 was discarded since it was with wrong labels of the ink marked/creased requirement

--- notebooks/supervised_unsupervised/local_config.py
+++ notebooks/supervised_unsupervised/local_config.py
@@ -2,4 +2,5 @@
 FOLDER_MODELS = 'models/unsupervised_supervised/'
 
 # files
+FILE_DATASET = 'data/inkmarked_creased/dataset.pkl'
 FILE_AUTOENCODER_SAMPLES = 'data/unsupervised_supervised_samples.pkl'
