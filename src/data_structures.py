3f513b7abe13b4565eddfb6647ed6562d5adafb4
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-02-13 17:55:56 -0300

    🗃️ generate dataset of normalized eye's positions

--- src/data_structures.py
+++ src/data_structures.py
@@ -8,14 +8,20 @@ class Point:
     def __str__(self):
         return '({}, {})'.format(self.x, self.y)
 
-    def __truediv__(self, other):
-        return Point(self.x / other.x, self.y / other.y)
-
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other):
+        return Point(self.x / other.x, self.y / other.y)
+
+    def as_list(self):
+        return [self.x, self.y]
 
 
 class Eye:
@@ -30,6 +36,9 @@ class Eye:
 
     def __str__(self):
         return 'left corner: {} right_corner: {}'.format(self.left_corner, self.right_corner)
+
+    def as_list(self):
+        return self.left_corner.as_list() + self.right_corner.as_list()
 
 
 class Rect():
