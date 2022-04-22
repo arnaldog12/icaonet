d32fc46f57572d88d1867ee2b5b3f8a713004550
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-04-22 14:55:48 -0300

    :sparkles: write a custom generator for landmarks

--- notebooks/supervised_unsupervised/local_config.py
+++ notebooks/supervised_unsupervised/local_config.py
@@ -2,5 +2,5 @@
 FOLDER_MODELS = 'models/unsupervised_supervised/'
 
 # files
-FILE_DATASET = 'data/cropped_faces_eyes/dataset_dlib_normalized.pkl'
+FILE_DATASET = 'data/cropped_faces_eyes/dataset_dlib.pkl'
 FILE_AUTOENCODER_SAMPLES = 'data/unsupervised_supervised_samples.pkl'
