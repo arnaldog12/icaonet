bfc06f7a550a07e0aef1d75ad127e1b91cbd9dcc
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-07-31 22:54:03 -0300

    submit v0.9.13 to FVC
    finetune of v0.6.4 + wing loss

---
+++ src/custom_losses.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import math
+
+
+def wing_loss(landmarks, labels, w=10.0, epsilon=2.0):
+    """
+    Arguments:
+        landmarks, labels: float tensors with shape [batch_size, 4].
+        w, epsilon: a float numbers.
+    Returns:
+        a float tensor with shape [].
+
+    Based on: https://github.com/TropComplique/wing-loss/blob/master/loss.py
+    """
+    with tf.name_scope('wing_loss'):
+        x = landmarks - labels
+        c = w * (1.0 - math.log(1.0 + w/epsilon))
+        absolute_x = tf.abs(x)
+        losses = tf.where(
+            tf.greater(w, absolute_x),
+            w * tf.log(1.0 + absolute_x/epsilon),
+            absolute_x - c
+        )
+        loss = tf.reduce_mean(tf.reduce_sum(losses, axis=-1), axis=0)
+        return loss
