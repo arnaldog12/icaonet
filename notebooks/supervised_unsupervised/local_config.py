69eff1c3cec4ff6d302a5dd3ee2d99a15bffcba7
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-10-15 10:54:57 -0300

    submit v0.9.15 to FVC
    landmarks of Celeb face dataset

--- notebooks/supervised_unsupervised/local_config.py
+++ notebooks/supervised_unsupervised/local_config.py
@@ -2,5 +2,4 @@
 FOLDER_MODELS = 'models/unsupervised_supervised/'
 
 # files
-FILE_DATASET = 'data/cropped_faces_eyes/dataset_dlib.pkl'
-FILE_AUTOENCODER_SAMPLES = 'data/unsupervised_supervised_samples.pkl'
+FILE_DATASET = 'data/celebfaces/dataset.pkl'
